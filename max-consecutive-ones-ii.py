cd4753e98cb08c4f24606f4d65f4e5e27a0f0221
tlkahn <joshgy@gmail.com>
2023-07-14 14:44:11 +0800

    Added alternative solutions for finding the maximum consecutive ones in an array
    Major changes:
    
    - Added SolutionB class with an alternative approach using a nested loop.
    
    - Added SolutionC class with a sliding window approach for finding the maximum consecutive ones

--- max-consecutive-ones-ii.py
+++ max-consecutive-ones-ii.py
@@ -32,3 +32,51 @@ class Solution:
             res = max(res, prev_zeros, prev_ones)
 
         return res
+
+
+class SolutionB:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        """
+        Not working yet.
+        """
+        N = len(nums)
+        if N < 2:
+            return N
+
+        i, j = 0, 0
+        k = 2
+        ans = float("-inf")
+
+        while i < N:
+            ones = 0
+            while j < N:
+                if nums[j] == 0:
+                    k -= 1
+                    if not k:
+                        break
+                j += 1
+                ones += 1
+            ans = max(ans, ones)
+            i = j + 1
+
+        return int(ans)
+
+
+class SolutionC:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        """
+        Sliding window
+        """
+        res = 0
+        count = 0
+
+        l = 0
+        for r in range(len(nums)):
+            if nums[r] == 0:
+                count += 1
+                while count > 1:
+                    count -= 1 if nums[l] == 0 else 0
+                    l += 1
+            res = max(res, r - l + 1)
+
+        return res
